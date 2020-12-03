43a737268dd539ba744738a7cdb8ef2bd4f25736
bentaro <swd.ben@gmail.com>
2020-12-03 22:55:31 +0900

    refactored contract

--- src/contract.rs
+++ src/contract.rs
@@ -1,10 +1,10 @@
 use crate::coin_helpers::assert_sent_sufficient_coin;
 
 use crate::msg::{
-    CreatePollResponse, HandleMsg, InitMsg, PollResponse, QueryMsg, TokenStakeResponse,
+    CreateListingResponse, HandleMsg, InitMsg, ListingResponse, QueryMsg, TokenStakeResponse,
 };
 use crate::state::{
-    bank, bank_read, config, config_read, poll, poll_read, Poll, PollStatus, State, Voter,
+    bank, bank_read, config, config_read, listing, listing_read, Listing, ListingStatus, State, Bidder,
 };
 use cosmwasm_std::{
     coin, to_binary, Api, Attribute, BankMsg, Binary, CanonicalAddr, Coin, CosmosMsg, Env, Extern,
@@ -28,7 +28,7 @@ pub fn init<S: Storage, A: Api, Q: Querier>(
     let state = State {
         denom: msg.denom.to_string(),
         owner: deps.api.canonical_address(&info.sender)?,
-        poll_count: 0,
+        listing_count: 0,
         staked_tokens: Uint128::zero(),
     };
 
@@ -44,27 +44,28 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
     msg: HandleMsg,
 ) -> StdResult<HandleResponse> {
     match msg {
-        HandleMsg::StakeVotingTokens {} => stake_voting_tokens(deps, env, info),
-        HandleMsg::WithdrawVotingTokens { amount } => withdraw_voting_tokens(deps, env, info, amount),
-        HandleMsg::CastVote {
-            poll_id,
-            vote,
-            weight,
-        } => cast_vote(deps, env, info, poll_id, vote, weight),
-        HandleMsg::EndPoll { poll_id } => end_poll(deps, env, info, poll_id),
-        HandleMsg::CreatePoll {
-            quorum_percentage,
-            description,
+        // HandleMsg::StakeVotingTokens {} => stake_voting_tokens(deps, env, info),
+        HandleMsg::WithdrawTokens { amount } => withdrawtokens(deps, env, info, amount),
+        HandleMsg::Bid {
+            listing_id,
+            price
+        } => bid(deps, env, info, listing_id, price),
+        HandleMsg::EndListing { listing_id } => end_listing(deps, env, info, listing_id),
+        HandleMsg::List {
+            token_id,
+            denom,
+            minimum_bid,
             start_height,
             end_height,
-        } => create_poll(
+            description,
+        } => create_listing(
             deps,
             env,
             info,
-            quorum_percentage,
-            description,
+            minimum_bid,
             start_height,
             end_height,
+            description,
         ),
         //handle for Nfts
         HandleMsg::GetNft { denom, id, } => get_nft(deps, env, info, denom, id),
@@ -105,7 +106,7 @@ pub fn stake_voting_tokens<S: Storage, A: Api, Q: Querier>(
 }
 
 // Withdraw amount if not staked. By default all funds will be withdrawn.
-pub fn withdraw_voting_tokens<S: Storage, A: Api, Q: Querier>(
+pub fn withdraw_tokens<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
     env: Env,
     info: MessageInfo,
@@ -171,102 +172,105 @@ fn validate_quorum_percentage(quorum_percentage: Option<u8>) -> StdResult<()> {
     }
 }
 
-/// validate_end_height returns an error if the poll ends in the past
+/// validate_end_height returns an error if the listing ends in the past
 fn validate_end_height(end_height: Option<u64>, env: Env) -> StdResult<()> {
     if end_height.is_some() && env.block.height >= end_height.unwrap() {
-        Err(StdError::generic_err("Poll cannot end in the past"))
+        Err(StdError::generic_err("Listing cannot end in the past"))
     } else {
         Ok(())
     }
 }
 
-/// create a new poll
-pub fn create_poll<S: Storage, A: Api, Q: Querier>(
+/// create a new listing
+pub fn create_listing<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
     env: Env,
     info: MessageInfo,
-    quorum_percentage: Option<u8>,
-    description: String,
+    minimum_bid : Uint128,
     start_height: Option<u64>,
     end_height: Option<u64>,
+    description: String,
 ) -> StdResult<HandleResponse> {
     validate_quorum_percentage(quorum_percentage)?;
     validate_end_height(end_height, env.clone())?;
     validate_description(&description)?;
 
     let mut state = config(&mut deps.storage).load()?;
-    let poll_count = state.poll_count;
-    let poll_id = poll_count + 1;
-    state.poll_count = poll_id;
+    let listing_count = state.listing_count;
+    let listing_id = listing_count + 1;
+    state.listing_count = listing_id;
 
     let sender_address_raw = deps.api.canonical_address(&info.sender)?;
-    let new_poll = Poll {
+    let token_id = &info.sent_nfts.id;
+    let denom = &info.sent_nfts.denom;
+
+    let new_listing = Listing {
+        token_id,
+        denom,
         creator: sender_address_raw,
-        status: PollStatus::InProgress,
-        quorum_percentage,
-        yes_votes: Uint128::zero(),
-        no_votes: Uint128::zero(),
-        voters: vec![],
-        voter_info: vec![],
-        end_height: end_height.unwrap_or(env.block.height + DEFAULT_END_HEIGHT_BLOCKS),
+        status: ListingStatus::InProgress,
+        highest_bid: Uint128::zero(),
+        minimum_bid,
+        bidders: vec![],
+        bidders_info: vec![],
         start_height,
+        end_height: end_height.unwrap_or(env.block.height + DEFAULT_END_HEIGHT_BLOCKS),
         description,
     };
-    let key = state.poll_count.to_string();
-    poll(&mut deps.storage).save(key.as_bytes(), &new_poll)?;
+    let key = state.listing_count.to_string();
+    listing(&mut deps.storage).save(key.as_bytes(), &new_listing)?;
 
     config(&mut deps.storage).save(&state)?;
 
     let r = HandleResponse {
         messages: vec![],
         attributes : vec![
-            Attribute { key:"action".to_string(), value:"create_poll".to_string(), },
-            Attribute { key: "creator".to_string(), value: deps.api.human_address(&new_poll.creator)?.to_string(), },
-            Attribute { key: "poll_id".to_string(), value: poll_id.to_string(), },
-            Attribute { key: "quorum_percentage".to_string(), value: quorum_percentage.unwrap_or(0).to_string(), },
-            Attribute { key: "end_height".to_string(), value: new_poll.end_height.to_string(), },
+            Attribute { key:"action".to_string(), value:"create_listing".to_string(), },
+            Attribute { key: "creator".to_string(), value: deps.api.human_address(&new_listing.creator)?.to_string(), },
+            Attribute { key: "listing_id".to_string(), value: listing_id.to_string(), },
+            Attribute { key: "end_height".to_string(), value: new_listing.end_height.to_string(), },
             Attribute { key: "start_height".to_string(), value: start_height.unwrap_or(0).to_string(), },
         ],
-        data: Some(to_binary(&CreatePollResponse { poll_id })?),
+        data: Some(to_binary(&CreateListingResponse { listing_id })?),
     };
     Ok(r)
 }
 
 /*
- * Ends a poll. Only the creator of a given poll can end that poll.
+ * Ends a listing. Only the creator of a given listing can end that listing.
  */
-pub fn end_poll<S: Storage, A: Api, Q: Querier>(
+pub fn end_listing<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
     env: Env,
     info: MessageInfo,
-    poll_id: u64,
+    listing_id: u64,
 ) -> HandleResult {
-    let key = &poll_id.to_string();
-    let mut a_poll = poll(&mut deps.storage).load(key.as_bytes())?;
+    let key = &listing_id.to_string();
+    let mut a_listing = listing(&mut deps.storage).load(key.as_bytes())?;
 
     let sender_address_raw = deps.api.canonical_address(&info.sender)?;
-    if a_poll.creator != sender_address_raw {
+    if a_listing.creator != sender_address_raw {
         return Err(StdError::generic_err(
-            "User is not the creator of the poll.",
+            "User is not the creator of the listing.",
         ));
     }
 
-    if a_poll.status != PollStatus::InProgress {
-        return Err(StdError::generic_err("Poll is not in progress"));
+    if a_listing.status != ListingStatus::InProgress {
+        return Err(StdError::generic_err("Listing is not in progress"));
     }
 
-    if a_poll.start_height.is_some() && a_poll.start_height.unwrap() > env.block.height {
+    if a_listing.start_height.is_some() && a_listing.start_height.unwrap() > env.block.height {
         return Err(StdError::generic_err("Voting period has not started."));
     }
 
-    if a_poll.end_height > env.block.height {
+    if a_listing.end_height > env.block.height {
         return Err(StdError::generic_err("Voting period has not expired."));
     }
 
     let mut no = 0u128;
     let mut yes = 0u128;
 
-    for voter in &a_poll.voter_info {
+    for voter in &a_listing.voter_info {
         if voter.vote == "yes" {
             yes += voter.weight.u128();
         } else {
@@ -275,7 +279,7 @@ pub fn end_poll<S: Storage, A: Api, Q: Querier>(
     }
     let tallied_weight = yes + no;
 
-    let poll_status = PollStatus::Rejected;
+    let listing_status = ListingStatus::Rejected;
     let mut rejected_reason = "";
     let mut passed = false;
 
@@ -294,14 +298,14 @@ pub fn end_poll<S: Storage, A: Api, Q: Querier>(
         }
 
         let quorum = ((tallied_weight / staked_weight) * 100) as u8;
-        if a_poll.quorum_percentage.is_some() && quorum < a_poll.quorum_percentage.unwrap() {
+        if a_listing.quorum_percentage.is_some() && quorum < a_listing.quorum_percentage.unwrap() {
             // Quorum: More than quorum_percentage of the total staked tokens at the end of the voting
             // period need to have participated in the vote.
             rejected_reason = "Quorum not reached";
         } else if yes > tallied_weight / 2 {
             //Threshold: More than 50% of the tokens that participated in the vote
             // (after excluding “Abstain” votes) need to have voted in favor of the proposal (“Yes”).
-            a_poll.status = PollStatus::Passed;
+            a_listing.status = ListingStatus::Passed;
             passed = true;
         } else {
             rejected_reason = "Threshold not reached";
@@ -309,16 +313,16 @@ pub fn end_poll<S: Storage, A: Api, Q: Querier>(
     } else {
         rejected_reason = "Quorum not reached";
     }
-    a_poll.status = poll_status;
-    poll(&mut deps.storage).save(key.as_bytes(), &a_poll)?;
+    a_listing.status = listing_status;
+    listing(&mut deps.storage).save(key.as_bytes(), &a_listing)?;
 
-    for voter in &a_poll.voters {
-        unlock_tokens(deps, voter, poll_id)?;
+    for voter in &a_listing.voters {
+        unlock_tokens(deps, voter, listing_id)?;
     }
 
     let attributes = vec![
-        Attribute { key: "action".to_string(), value: "end_poll".to_string(), },
-        Attribute { key: "poll_id".to_string(), value: poll_id.to_string(), },
+        Attribute { key: "action".to_string(), value: "end_listing".to_string(), },
+        Attribute { key: "listing_id".to_string(), value: listing_id.to_string(), },
         Attribute { key: "rejected_reason".to_string(), value: rejected_reason.to_string(), },
         Attribute { key: "passed".to_string(), value: passed.to_string(), },
     ];
@@ -331,22 +335,22 @@ pub fn end_poll<S: Storage, A: Api, Q: Querier>(
     Ok(r)
 }
 
-// unlock voter's tokens in a given poll
+// unlock voter's tokens in a given listing
 fn unlock_tokens<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
     voter: &CanonicalAddr,
-    poll_id: u64,
+    listing_id: u64,
 ) -> HandleResult {
     let voter_key = &voter.as_slice();
     let mut token_manager = bank_read(&deps.storage).load(voter_key).unwrap();
 
-    // unlock entails removing the mapped poll_id, retaining the rest
-    token_manager.locked_tokens.retain(|(k, _)| k != &poll_id);
+    // unlock entails removing the mapped listing_id, retaining the rest
+    token_manager.locked_tokens.retain(|(k, _)| k != &listing_id);
     bank(&mut deps.storage).save(voter_key, &token_manager)?;
     Ok(HandleResponse::default())
 }
 
-// finds the largest locked amount in participated polls.
+// finds the largest locked amount in participated listings.
 fn locked_amount<S: Storage, A: Api, Q: Querier>(
     voter: &CanonicalAddr,
     deps: &mut Extern<S, A, Q>,
@@ -361,32 +365,31 @@ fn locked_amount<S: Storage, A: Api, Q: Querier>(
         .unwrap_or_default()
 }
 
-fn has_voted(voter: &CanonicalAddr, a_poll: &Poll) -> bool {
-    a_poll.voters.iter().any(|i| i == voter)
+fn has_voted(voter: &CanonicalAddr, a_listing: &Listing) -> bool {
+    a_listing.voters.iter().any(|i| i == voter)
 }
 
-pub fn cast_vote<S: Storage, A: Api, Q: Querier>(
+pub fn bid<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
     _env: Env,
     info: MessageInfo,
-    poll_id: u64,
-    vote: String,
-    weight: Uint128,
+    listing_id: u64,
+    price: Uint128,
 ) -> HandleResult {
     let sender_address_raw = deps.api.canonical_address(&info.sender)?;
-    let poll_key = &poll_id.to_string();
+    let listing_key = &listing_id.to_string();
     let state = config_read(&deps.storage).load()?;
-    if poll_id == 0 || state.poll_count > poll_id {
-        return Err(StdError::generic_err("Poll does not exist"));
+    if listing_id == 0 || state.listing_count > listing_id {
+        return Err(StdError::generic_err("Listing does not exist"));
     }
 
-    let mut a_poll = poll(&mut deps.storage).load(poll_key.as_bytes())?;
+    let mut a_listing = listing(&mut deps.storage).load(listing_key.as_bytes())?;
 
-    if a_poll.status != PollStatus::InProgress {
-        return Err(StdError::generic_err("Poll is not in progress"));
+    if a_listing.status != ListingStatus::InProgress {
+        return Err(StdError::generic_err("Listing is not in progress"));
     }
 
-    if has_voted(&sender_address_raw, &a_poll) {
+    if has_voted(&sender_address_raw, &a_listing) {
         return Err(StdError::generic_err("User has already voted."));
     }
 
@@ -398,20 +401,20 @@ pub fn cast_vote<S: Storage, A: Api, Q: Querier>(
             "User does not have enough staked tokens.",
         ));
     }
-    token_manager.participated_polls.push(poll_id);
-    token_manager.locked_tokens.push((poll_id, weight));
+    token_manager.participated_listings.push(listing_id);
+    token_manager.locked_tokens.push((listing_id, price));
     bank(&mut deps.storage).save(key, &token_manager)?;
 
-    a_poll.voters.push(sender_address_raw.clone());
+    a_listing.bidders.push(sender_address_raw.clone());
 
-    let voter_info = Voter { vote, weight };
+    let bidder_info = Bidder { sender_address_raw, price};
 
-    a_poll.voter_info.push(voter_info);
-    poll(&mut deps.storage).save(poll_key.as_bytes(), &a_poll)?;
+    a_listing.voter_info.push(voter_info);
+    listing(&mut deps.storage).save(listing_key.as_bytes(), &a_listing)?;
 
     let attributes = vec![
         Attribute { key: "action".to_string(), value: "vote_casted".to_string(), },
-        Attribute { key: "poll_id".to_string(), value:  poll_id.to_string(), },
+        Attribute { key: "listing_id".to_string(), value:  listing_id.to_string(), },
         Attribute { key: "weight".to_string(), value: weight.to_string(), },
         Attribute { key: "voter".to_string(), value: info.sender.to_string(), },
     ];
@@ -457,30 +460,30 @@ pub fn query<S: Storage, A: Api, Q: Querier>(
         QueryMsg::Config {} => to_binary(&config_read(&_deps.storage).load()?),
 
         QueryMsg::TokenStake { address } => token_balance(_deps, address),
-        QueryMsg::Poll { poll_id } => query_poll(_deps, poll_id),
+        QueryMsg::Listing { listing_id } => query_listing(_deps, listing_id),
     }
 }
 
-fn query_poll<S: Storage, A: Api, Q: Querier>(
+fn query_listing<S: Storage, A: Api, Q: Querier>(
     deps: &Extern<S, A, Q>,
-    poll_id: u64,
+    listing_id: u64,
 ) -> StdResult<Binary> {
-    let key = &poll_id.to_string();
-//poll_readはstateにて定義、バイナリをオブジェクト化
-    let poll = match poll_read(&deps.storage).may_load(key.as_bytes())? {
+    let key = &listing_id.to_string();
+//listing_readはstateにて定義、バイナリをオブジェクト化
+    let listing = match listing_read(&deps.storage).may_load(key.as_bytes())? {
 //型マッチしていれば返す
-        Some(poll) => Some(poll),
-        None => return Err(StdError::generic_err("Poll does not exist")),
+        Some(listing) => Some(listing),
+        None => return Err(StdError::generic_err("Listing does not exist")),
     }
     .unwrap();
-//pollオブジェクトの情報とメタデータからオブジェクト生成
-    let resp = PollResponse {
-        creator: deps.api.human_address(&poll.creator).unwrap(),
-        status: poll.status,
-        quorum_percentage: poll.quorum_percentage,
-        end_height: Some(poll.end_height),
-        start_height: poll.start_height,
-        description: poll.description,
+//listingオブジェクトの情報とメタデータからオブジェクト生成
+    let resp = ListingResponse {
+        creator: deps.api.human_address(&listing.creator).unwrap(),
+        status: listing.status,
+        quorum_percentage: listing.quorum_percentage,
+        end_height: Some(listing.end_height),
+        start_height: listing.start_height,
+        description: listing.description,
     };
 //バイナリで返す
     to_binary(&resp)
