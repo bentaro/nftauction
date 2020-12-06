8ecc5e8c5d2857480f818435867f84db6626ce8d
bentaro <swd.ben@gmail.com>
2020-12-06 18:38:38 +0900

    test contract

---
+++ target/wasm32-unknown-unknown/release/auction.d
@@ -0,0 +1,1 @@
+/work/nftauction/target/wasm32-unknown-unknown/release/auction.wasm: /work/nftauction/src/coin_helpers.rs /work/nftauction/src/contract.rs /work/nftauction/src/lib.rs /work/nftauction/src/msg.rs /work/nftauction/src/state.rs
---
+++ target/wasm32-unknown-unknown/release/deps/auction.d
@@ -0,0 +1,11 @@
+/work/nftauction/target/wasm32-unknown-unknown/release/deps/auction.wasm: src/lib.rs src/coin_helpers.rs src/contract.rs src/msg.rs src/state.rs
+
+/work/nftauction/target/wasm32-unknown-unknown/release/deps/libauction.rlib: src/lib.rs src/coin_helpers.rs src/contract.rs src/msg.rs src/state.rs
+
+/work/nftauction/target/wasm32-unknown-unknown/release/deps/auction.d: src/lib.rs src/coin_helpers.rs src/contract.rs src/msg.rs src/state.rs
+
+src/lib.rs:
+src/coin_helpers.rs:
+src/contract.rs:
+src/msg.rs:
+src/state.rs:
